efecf016b7ad27c3d18cc95ec23a49838db4ee20
Nitya2001 <81946328+Nitya2001@users.noreply.github.com>
2021-12-25 02:36:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,7 @@
 </head>
 
 <body>
-    <div>
-        
+    <center>
         <fieldset id="group1">
             <h1>Aromatica Spa</h1>
             <div>
@@ -19,7 +18,6 @@
                     <li>Aromatica Spa</li>
                     <li>Kangar, Perlis</li>
                     <li>03-2003444</li>
-                    <li>aromaticaspa.com</li>
                 </ul>
                 We locally owned and operated Aromatica Spa! Outstanding spa
                 treatments in Facials, Massages, Body Treatment, Makeup,
@@ -30,79 +28,88 @@
             </div>
 
         </fieldset>
-    </div> <br>
+    </center>
 
-    <center>
+    <br>
 
-    <form id="orderProduct" method="post" action="result.php">
-    <table border="1">
-            <tr>
-                <td>
-                    <h1>Massage RM50 +</h1>
-                    
-                </td>
-                <td>
-                    <input type="checkbox" name="cb1" value="1" id="one">
-                    <label>Organic Foot Scrub Rm30</label><br>
-                    <input type="checkbox" name="cb2" value="2" id="two">
-                    <label>Cold Stone Facial Massage RM30</label><br>
-                    <input type="checkbox" name="cb3" value="2" id="three">
-                    <label>Scalp Massage with Organic Apricot Oil RM30</label><br>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <h1>Facials RM70+</h1>
-                    
-                </td>
-                <td>
-                    <input type="checkbox" name="cb4" value="1" id="one">
-                    <label>Lip & Eye Treatment Rm40</label><br>
-                    <input type="checkbox" name="cb5" value="2" id="two">
-                    <label>Lash Tinting RM20</label><br>
-                    <input type="checkbox" name="cb6" value="2" id="three">
-                    <label>Eyebrow Tinting RM20</label><br>
-                </td>
-            </tr>
 
-            <tr>
-                <td>
-                    <h1>Waxing and Threading RM40 +</h1>
-                    
-                </td>
-                <td>
-                    <input type="checkbox" name="cb7" value="1" id="one">
-                    <label>Arm RM50</label><br>
-                    <input type="checkbox" name="cb8" value="2" id="two">
-                    <label>Back RM100</label><br>
-                    <input type="checkbox" name="cb9" value="2" id="three">
-                    <label>Legs RM60</label><br>
-                </td>
+    <div>
 
-                <tr>
-                <td>
-                    <h1>Makeup RM60 +</h1>
-                    
-                </td>
-                <td>
-                    <input type="checkbox" name="cb11" value="1" id="one">
-                    <label>Lash Tint Rm20</label><br>
-                    <input type="checkbox" name="cb12" value="2" id="two">
-                    <label>Eyebrow Tint RM20</label><br>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="6">
-                    <center>
-                    <button type="submit">Pay</button>
-                    <button type="reset">Reset</button>
-                    <center>
-                </td>
-            </tr>
-           
-        </table>
-    </form>
-    <center>
+        <center>
+            <form id="bookService" method="post" action="result.php">
+                <table>
+                    <tr>
+                        <td>
+                            <input type="radio" name="rad" id="rad1" value="Massage RM50">
+                            Massage RM50 +
+                        </td>
+                        <td>
+                            <input type="checkbox" name="cb1" value="30" id="one">
+                            <label>Organic Foot Scrub Rm30</label><br>
+                            <input type="checkbox" name="cb2" value="30" id="two">
+                            <label>Cold Stone Facial Massage RM30</label><br>
+                            <input type="checkbox" name="cb3" value="30" id="three">
+                            <label>Scalp Massage with Organic Apricot Oil RM30</label><br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="radio" name="rad" id="rad2" value="Facials RM70">
+                            Facials RM70 +
+
+                        </td>
+                        <td>
+                            <input type="checkbox" name="cb4" value="40" id="one">
+                            <label>Lip & Eye Treatment Rm40</label><br>
+                            <input type="checkbox" name="cb5" value="20" id="two">
+                            <label>Lash Tinting RM20</label><br>
+                            <input type="checkbox" name="cb6" value="20" id="three">
+                            <label>Eyebrow Tinting RM20</label><br>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <input type="radio" name="rad" id="rad3" value="Waxing & Threading RM40">
+                            Waxing & Threading RM40 +
+
+                        </td>
+                        <td>
+                            <input type="checkbox" name="cb7" value="50" id="one">
+                            <label>Arm RM50</label><br>
+                            <input type="checkbox" name="cb8" value="100" id="two">
+                            <label>Back RM100</label><br>
+                            <input type="checkbox" name="cb9" value="60" id="three">
+                            <label>Legs RM60</label><br>
+                        </td>
+
+                    <tr>
+                        <td>
+                            <input type="radio" name="rad" id="rad4" value="Makeup RM60">
+                            Makeup RM60 +
+
+                        </td>
+                        <td>
+                            <input type="checkbox" name="cb10" value="20" id="one">
+                            <label>Lash Tint Rm20</label><br>
+                            <input type="checkbox" name="cb11" value="20" id="two">
+                            <label>Eyebrow Tint RM20</label><br>
+                            <input type="checkbox" name="cb12" value="35" id="three">
+                            <label>Last & Eyebrow Tint RM35</label><br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="6">
+                            <center>
+                                <button type="submit">Pay</button>
+                                <button type="reset">Reset</button>
+                                <center>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+            <center>
+    </div>
 </body>
 
 </html>
